530550107983420853db03120c4b24875c65402d
Dileeshana <dileeshana2000@gmail.com>
2023-05-10 08:03:16 +0530

    add routing and services

---
+++ house-app/src/app/housing.service.ts
@@ -0,0 +1,119 @@
+import { Injectable } from '@angular/core';
+import { Housinglocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  protected housingLocationList: Housinglocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/images/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/images/ian-macdonald-W8z6aiwfi1E-unsplash.jpeg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '/assets/images/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/images/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '/assets/images/photo-1515263487990-61b07816b324.jpeg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/images/r-architecture-GGupkreKwxA-unsplash.jpeg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/images/photo-1593398395073-ae53c3870037.jpg',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/images/r-architecture-JvQ0Q5IkeMM-unsplash.jpeg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/images/saru-robert-9rP3mxf8qWI-unsplash.jpeg',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '/assets/images/webaliser-_TPTXZd9mOo-unsplash.jpeg',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true
+    }
+  ];
+  constructor() { }
+
+  getAllHousingLocations() : Housinglocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: Number) : Housinglocation | undefined {
+    return this.housingLocationList.find(housingLocation =>housingLocation.id === id);
+  }
+}
